fa5b4c254fec82bd618e623b169deed3abb5fc33
[Ian Rutagengwa] <[iansheja56@gmail.com]>
2026-08-28 07:07:42 -0400

    Final flask impmentation on chart

--- chart.py
+++ chart.py
@@ -1,14 +1,19 @@
 import matplotlib
-matplotlib.use("Agg") # sets Matplotlib backend to a non-interactive, non-GUI rendering engine will render in memory/hence no pop-up
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np 
 import sqlite3
 import pandas as pd 
+import io
+import base64
 
 def main():
 
-   get_graph()
+    correlation, image_base64 = get_graph("recession","unemployment benefits")
+    print(correlation)
+    print(len(image_base64))
 
+  
 def get_graph(term_a, term_b):
     with sqlite3.connect("data/trends.db") as connect:
         df = pd.read_sql_query(
@@ -20,51 +25,62 @@ def get_graph(term_a, term_b):
     wide = df.pivot(index="date", columns="term", values="value")
     correlation = wide[term_a].corr(wide[term_b])
 
-    x_numeric = np.array(df.index.map(lambda d: d.toordinal()))
+    x_numeric = np.array(wide.index.map(lambda d: d.toordinal()))
 
     M = 3 # the polynomial curve 
 
     coeffs_a = np.polyfit(x_numeric, wide[term_a], M)# find the best fot curve(degree 3) that matches x-value(x_numeric) and then y-value(wide[term(a)])
     poly_a = np.polyval(coeffs_a, x_numeric) # now calculate the y-values at each pf this x-value points 
 
-    # similar thing to be done for the secon term 
-    coeffs_b = np.polyfit(x_numeric, wide[term_a], M)
+    # similar thing to be done for the second term 
+    coeffs_b = np.polyfit(x_numeric, wide[term_b], M)
     poly_b = np.polyval(coeffs_b, x_numeric)
 
     #create the overall figure and one axis to start 
-    fig, ax_left = plt.subplot(figsize=(12,6))#12 wide and 6 tall
+    fig, ax_left = plt.subplots(figsize=(12,6))#12 wide and 6 tall
 
-    # plot your data point(dates on x, terms values on y) and also customization 
+    # plot your data point(dates on x, terms values on y and also customization 
 
     #first for the y-axis 
     ax_left.scatter(wide.index, wide[term_a], color = "tab:blue", s = 45, alpha = 0.8, edgecolor = "black", label = term_a) # in this specfic area draw individual dots 
-    ax_left.plot(wide.index, poly_a, color = "blue", linewidth=2, label=f"Fit:({term_a})") #draw a connected line instead of just seperated dots, this is the polynomial fit curve
-    ax_left.set_ylabel(term_a, color="tab:blue") # label the left axis and give it its color 
+    ax_left.plot(wide.index, poly_a, color = "blue", linewidth=2, label=f"Fit:{term_a.capitalize()}") #draw a connected line instead of just seperated dots, this is the polynomial fit curve
+    ax_left.set_ylabel(term_a.capitalize(), color="tab:blue") # label the left axis and give it its color 
     ax_left.tick_params(axis="y", labelcolor = "tab:blue") # the color of the tick marks along the y axis 
 
     # then for the x-axis 
-    ax_right = ax_left.twin() # this creates another axis but the key is this make sit that is shared with the first axis 
-    ax_right.scatter(wide.index, wide[term_a], color = "tab:red", s = 45, alpha = 0.8, edgecolor = "black", label = term_b)
-    ax_right.plot(wide.index, poly_b, color = "red", linewidet = 2, label = f"Fit:({term_b})")
-    ax_right.set_xlabel(term_b, color="tab:red")
-    ax_right.teck_params(axis="x", labelcolor = "tab:red")
+    ax_right = ax_left.twinx() # new axis that shares the x-axis with ax_left but has its own y-axis is what were doing 
+    ax_right.scatter(wide.index, wide[term_b], color = "tab:red", s = 45, alpha = 0.8, edgecolor = "black", label = term_b)
+    ax_right.plot(wide.index, poly_b, color = "red", linewidth = 2, label = f"Fit:{term_b.capitalize()}")
+    ax_right.set_ylabel(term_b.capitalize(), color="tab:red")
+    ax_right.tick_params(axis="y", labelcolor = "tab:red")
 
     # add a title for the graph 
-    plt.title(f"{term_a} vs {term_b}")
+    plt.title(f"{term_a.capitalize()} vs {term_b.capitalize()}", weight = "bold")
 
-    # add the legend to the graph 
-    lines1, labels1 = ax_left.get_legend_handles_labels()
-    lines2, labels2 = ax_right.get_legend_handles_labels()
-    ax_left.legnet(lines1 + lines2, labels1 + labels2, loc="upper left", fontsize = 10)
+    # add the legend to the graph
+    # we need to use .get_legend_handles() so the two acis can know about each other 
+        #.get_legend_handles() ask out of everything drawn get the visual obkecs and the text name
+    lines1, labels1 = ax_left.get_legend_handles_labels()  #  labels is [term_a, f"Fit ({term_a})"]
+    lines2, labels2 = ax_right.get_legend_handles_labels() 
+
+
+    # then we creat the legned with both the lines and labes together 
+    ax_left.legend(lines1 + lines2, labels1 + labels2, loc="upper right", fontsize = 10)
 
     # add the correlation label 
-    fig.text(0.5, 0.02, f"Pearson Correlation coefficientL r = {correlation}") # places text anywhere based of the x & y cordinates 
+    fig.text(0.33, 0.03, f"Pearson Correlation coefficient: r = {correlation}") # places text anywhere based of the x & y cordinates 
 
     # layout adjustment/ nuge the plotting area so the bottome correlation text sits outside the plot area
     fig.tight_layout(rect=[0, 0.05, 1, 0.95])
 
+    #display the graph 
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png")
+    plt.close(fig)
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
 
-
+    return correlation, image_base64
 
 if __name__ == "__main__":
     main() 
--- fetch_data.py
+++ fetch_data.py
@@ -11,11 +11,9 @@ cursor = connect.cursor()
 # create a column witht eh table values we want
 cursor.execute("CREATE TABLE IF NOT EXISTS data(date, term, value, PRIMARY KEY (date, term))")
 
-# we are first going to make sure the table was created
 
 # connect to Google with an object
 pytrends = TrendReq(hl="en-US", tz=360, timeout=(20, 25))
-
 
 # Build payload/ the core data transmitted
 kw_list = ["unemployment benefits", "flu symptoms", "recession", "pumpkin spice"]
@@ -23,20 +21,15 @@ kw_list = ["unemployment benefits", "flu symptoms", "recession", "pumpkin spice"
 pytrends.build_payload(kw_list, timeframe="today 5-y", geo="US")
 
 # check if the table has actually been created and if not exit out of the program
-
 res = cursor.execute("SELECT name FROM sqlite_master WHERE name = 'data'")
 result = res.fetchone()
 
 if result is None or result[0] != "data":
     sys.exit("Table was not created")
-
-
 # print the pytrends intrest overtime and see what we get
-
 data = pytrends.interest_over_time()
 
 # No we are going to put the values we recive from pytrends to the table
-
 for date, row in data.iterrows():
 
     # to get the data we put the
